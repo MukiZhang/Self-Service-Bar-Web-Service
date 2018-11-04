c01c32c93fe24331815ce6b3a0610e40521e59b3
Muki <821404356@qq.com>
2018-11-04 21:46:50 +0800

    Construct Architecture

---
+++ src/main/java/com/SelfServiceBarWeb/SelfServiceBarWebApplication.java
@@ -0,0 +1,25 @@
+package com.SelfServiceBarWeb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Created by Muki on 2018/11/4
+ */
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAsync
+@MapperScan("com.SelfServiceBarWebApplication.mapper")
+public class SelfServiceBarWebApplication {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(SelfServiceBarWebApplication.class);
+        logger.debug("start application");
+        SpringApplication.run(SelfServiceBarWebApplication.class, args);
+    }
+}
