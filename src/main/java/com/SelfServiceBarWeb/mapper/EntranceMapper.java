b65c24386d20d2f4ad198cc26266ef04a5285f83
Muki <821404356@qq.com>
2018-11-11 23:19:40 +0800

    Implement part apis of 'EntranceService'

---
+++ src/main/java/com/SelfServiceBarWeb/mapper/EntranceMapper.java
@@ -0,0 +1,13 @@
+package com.SelfServiceBarWeb.mapper;
+
+import com.SelfServiceBarWeb.model.Entrance;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by Muki on 2018/11/10
+ */
+public interface EntranceMapper {
+    //目前暂定一个门禁
+    @Select("SELECT * FROM entrance_info;")
+    Entrance getEntranceInfo();
+}
