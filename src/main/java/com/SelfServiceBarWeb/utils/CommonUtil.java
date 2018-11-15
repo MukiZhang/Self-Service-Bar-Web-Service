585f2e6e5f62494fb5b9b23751dff716c6b21936
Muki <821404356@qq.com>
2018-11-15 17:47:32 +0800

    Add method to send post request

--- src/main/java/com/SelfServiceBarWeb/utils/CommonUtil.java
+++ src/main/java/com/SelfServiceBarWeb/utils/CommonUtil.java
@@ -2,13 +2,20 @@ package com.SelfServiceBarWeb.utils;
 
 import com.SelfServiceBarWeb.constant.ResponseMessage;
 import com.SelfServiceBarWeb.model.SelfServiceBarWebException;
+import com.SelfServiceBarWeb.model.response.HttpResponseContent;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
 import java.util.Date;
 
@@ -42,5 +49,25 @@ public class CommonUtil {
             throw new SelfServiceBarWebException(403, ResponseMessage.ERROR, message);
         }
         return jwt;
+    }
+
+    public static HttpResponseContent sendPost(String url, String body) {
+        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead
+        HttpResponseContent httpResponseContent = new HttpResponseContent();
+        try {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity(body);
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+
+            httpResponseContent.setResponseCode(response.getStatusLine().getStatusCode());
+            HttpEntity entity = response.getEntity();
+            String result = EntityUtils.toString(entity);
+            httpResponseContent.setContent(result);
+        } catch (Exception ex) {
+            throw new SelfServiceBarWebException(500, ResponseMessage.ERROR, ResponseMessage.INNER_SERVER_ERROR);
+        }
+        return httpResponseContent;
     }
 }
