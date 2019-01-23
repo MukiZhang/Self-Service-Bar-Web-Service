86e6cf9ec72ca7d061a2a113c54cca318384c295
waitrysb <2295933726@qq.com>
2019-01-23 13:35:44 +0800

    new

--- src/main/java/com/SelfServiceBarWeb/SelfServiceBarWebApplication.java
+++ src/main/java/com/SelfServiceBarWeb/SelfServiceBarWebApplication.java
@@ -2,8 +2,8 @@ package com.SelfServiceBarWeb;
 
 import com.SelfServiceBarWeb.server.SocketServer;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -21,8 +21,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SelfServiceBarWebApplication {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(SelfServiceBarWebApplication.class);
-        logger.debug("start application");
+        //Logger logger = LoggerFactory.getLogger(SelfServiceBarWebApplication.class);
+        //logger.debug("start application");
         SpringApplication.run(SelfServiceBarWebApplication.class, args);
 
         //启动socket服务
